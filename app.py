1d47d58d4140b73c680ff5dc0b90776452b09f4f
Raytang88 <tc251369779@163.com>
2024-04-18 20:06:25 +0800

    init code

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ os.system('apt install git-lfs')
 os.system(f'git clone https://code.openxlab.org.cn/raytang88/internlm2-1_8b-pet-knowledge-assistant-model.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
-backend_config = TurbomindEngineConfig(session_len=8192, model_fomat='hf')
-
-pipe = pipeline(base_path, backend_config=backend_config)
-
+backend_config = TurbomindEngineConfig(session_len=8192)
+# base_path = "/data0/tc_workspace/internlm/model/internlm2-1_8b-pet-knowledge-assistant-model/"
+# base_path = "/data0/tc_workspace/internlm/code/tutorial/xtuner/ft-oasst1/work_dirs/hf_merge/"
+pipe = pipeline(base_path, model_name="internlm2-1_8b-pet-knowledge-assistant-model", backend_config=backend_config)
+# pipe = pipeline(base_path, model_name="internlm2-chat-1_8b")
 def model(text):
     if text is None:
         return [(text, "请输入你的问题。")]
